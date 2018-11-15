126e9214186e10d26f2be05696031b0f468ffe76
Padhma <padhmasahithya@gmail.com>
2018-11-15 19:50:45 +0530

    Feat:Added the enemy class

--- js/app.js
+++ js/app.js
@@ -1,25 +1,43 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    constructor(x,y,speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+     update(dt) {
+        if (this.x > 404) {
+            this.x = 0;
+        }
+        else {
+            this.x += this.speed * dt;
+        }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+        if (player.x < this.x + 80 && player.x + 80 > this.x &&
+            player.y < this.y + 60 && player.y + 60 > this.y) {
+            alert('Uh-Oh! You lost!');
+            //player.resetPlayerPosition();
+        }
+    }
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
+
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
